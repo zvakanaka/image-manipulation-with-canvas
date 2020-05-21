0ea2f8d9a37692c2c6f2941a4e2aa61d1d560e2b
zvakanaka <adam.quinton@familysearch.org>
2020-05-21 12:58:43 -0600

    webcam and file upload support

--- index.js
+++ index.js
@@ -2,43 +2,44 @@ import imageFilters from './js/imageFilters/index.js'
 import createControl from './js/createControl.js'
 import download from './js/download.js'
 import { corsServer } from './config.js'
+import fileParts from './js/fileParts.js'
 
-const imageSrc = 'https://images.unsplash.com/photo-1579036018199-6cab68e7f7c9?ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80'
-const exampleSrc = `${corsServer}${imageSrc}`
+const inputSourceSelect = document.querySelector('.input-source-select')
+const imageFilterContainer = document.querySelector('.image-filter-container')
+const imageFilterSelect = imageFilterContainer.querySelector('.image-filter-select')
+const imageFilterControls = imageFilterContainer.querySelector('.image-filter-controls')
+const drop = document.querySelector('.drop')
+const imageInput = drop.querySelector('#image-file')
 
 const state = {
   ratio: null,
   canvas: document.querySelector('canvas') // defaults to 300x150
 }
 
-state.image = new Image()
-state.image.crossOrigin = 'anonymous'
-state.image.src = exampleSrc
 state.ctx = state.canvas.getContext('2d')
 
 state.drawImg = () => {
   // contain image within view
   const maxWidth = window.innerWidth
   const maxHeight = window.innerHeight
+  const imageWidth = state.image.videoWidth || state.image.width
+  const imageHeight = state.image.videoHeight || state.image.height
+
   if (maxWidth < maxHeight) {
-    if (state.image.width > maxWidth) {
+    if (imageWidth > maxWidth) {
       state.canvas.width = maxWidth
-      state.ratio = state.image.width / maxWidth
-      state.canvas.height = state.image.height / state.ratio
+      state.ratio = imageWidth / maxWidth
+      state.canvas.height = imageHeight / state.ratio
     }
   } else {
-    if (state.image.height > maxHeight) {
+    if (imageHeight > maxHeight) {
       state.canvas.height = maxHeight
-      state.ratio = state.image.height / maxHeight
-      state.canvas.width = state.image.width / state.ratio
+      state.ratio = imageHeight / maxHeight
+      state.canvas.width = imageWidth / state.ratio
     }
   }
   state.ctx.drawImage(state.image, 0, 0, state.canvas.width, state.canvas.height)
 }
-
-const imageFilterContainer = document.querySelector('.image-filter-container')
-const imageFilterSelect = imageFilterContainer.querySelector('.image-filter-select')
-const imageFilterControls = imageFilterContainer.querySelector('.image-filter-controls')
 
 const addImageFilter = (name, i) => {
   const option = document.createElement('option')
@@ -71,8 +72,6 @@ imageFilterSelect.addEventListener('change', () => {
   delete state.lastFuncArg
   draw()
 })
-state.image.onload = draw
-window.onresize = draw
 
 state.canvas.addEventListener('mousedown', ({layerX, layerY}) => {
   state.lastClickX = layerX
@@ -89,6 +88,117 @@ state.canvas.addEventListener('mousemove', ({layerX, layerY}) => {
     draw()
   }
 })
+
+window.onresize = () => {
+  draw()
+}
+
+function consumeFile(ev) {
+  const target = ev.dataTransfer ? ev.dataTransfer : ev.target;
+  window.fileObject = target.files[0];
+  if (target.files.length === 1) { // single file
+    //determine if our URL is a supported image
+    const supportedFileExtensions = ['png', 'jpg', 'gif', 'bmp', 'jpeg'];
+    const {ext} = fileParts(target.files[0].name.toLowerCase());
+    if (supportedFileExtensions.includes(ext)) { // valid extension
+      loadFile(target.files);
+    } else {
+      alert(`Invalid extension ${ext} in ${supportedFileExtensions.join(', ')}`);
+    }
+  }
+  else { // list of files
+    alert('No support for list of files yet');
+  }
+}
+
+imageInput.addEventListener('change', (ev) => {
+  ev.preventDefault()
+  consumeFile(ev)
+})
+
+document.addEventListener('drop', (ev) => {
+  ev.preventDefault()
+  consumeFile(ev)
+}, false)
+
+document.addEventListener('dragover', (ev) => ev.preventDefault())
+
+function loadImage() {
+  const imageSrc = 'https://images.unsplash.com/photo-1579036018199-6cab68e7f7c9?ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80'
+  const exampleSrc = `${corsServer}${imageSrc}`
+  state.image = new Image()
+  state.image.crossOrigin = 'anonymous'
+  state.image.src = exampleSrc
+  state.image.onload = draw
+}
+
+function loadFile(files) {
+  let file, fr
+
+  if (typeof window.FileReader !== 'function') {
+    console.log('File API not supported')
+    return
+  }
+
+  if (!files) {
+    console.log('No support for files input')
+  } else if (!files[0]) {
+    console.log('No file selected')
+  } else {
+    file = files[0]
+    fr = new FileReader()
+    fr.onload = () => {
+      state.image = new Image()
+      state.image.onload = () => {
+        draw()
+      }
+      state.image.src = fr.result
+    }
+    fr.readAsDataURL(file)
+  }
+}
+
+async function loadVideo() {
+  state.video = document.querySelector('video')
+  const mediaStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+  state.video.srcObject = mediaStream
+  state.video.play()
+  state.video.addEventListener('canplay', () => {
+    state.image = state.video
+    state.videoIntervalHandle = setInterval(draw, 16)
+  })
+}
+
+function stopVideo() {
+  clearInterval(state.videoIntervalHandle)
+  state.video.pause()
+  if (state.video.srcObject) {
+    state.video.srcObject.getTracks().forEach(track => track.stop())
+  }
+  state.video.srcObject = null
+  delete state.image.videoWidth
+  delete state.image.videoHeight
+}
+
+inputSourceSelect.addEventListener('change', () => {
+  drop.hidden = true;
+  switch (inputSourceSelect.value) {
+    case 'webcam':
+      loadVideo()
+      break
+    case 'url':
+      stopVideo()
+      loadImage()
+      break
+    case 'file':
+      stopVideo()
+      drop.hidden = false;
+      break
+    default:
+      break
+  }
+})
+loadVideo()
 
 // export button - or you can right-click the canvas -> save image
 const exportButton = document.querySelector('.export')
