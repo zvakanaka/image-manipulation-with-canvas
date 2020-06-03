52cfb5788ccb7eef94ad506cba7d28e73a2988da
Adam Quinton <quintonish@gmail.com>
2020-06-03 10:00:26 -0700

    comment out imports

--- js/imageFilters/index.js
+++ js/imageFilters/index.js
@@ -1,19 +1,19 @@
 import blackAndWhite from './blackAndWhite.js'
 import grayscale from './grayscale.js'
 import invert from './invert.js'
-import magnify from './magnify.js'
+// import magnify from './magnify.js'
 import noFilter from './noFilter.js'
 import pixelate from './pixelate.js'
-import pointilism from './pointilism.js'
+// import pointilism from './pointilism.js'
 import scriptureVision from './scriptureVision.js'
 
 export default [
   blackAndWhite,
   grayscale,
   invert,
-  magnify,
+  // magnify,
   noFilter,
   pixelate,
-  pointilism,
+  // pointilism,
   scriptureVision
 ]
