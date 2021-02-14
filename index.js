5078573b3e3cc77c3623f977e1017659a336c85f
Adam Quinton <quintonish@gmail.com>
2021-02-13 22:47:43 -0700

    add more commands and ui

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import initDragAndDrop from './js/dragAndDrop.js'
 import { corsServer } from './config.js'
 import './js/logWindow.js'
 
+const ENDPOINT = ''
 // document.body.appendChild(document.createElement('log-window'))
 // customElements.whenDefined('log-window').then(() => {
 //   const log = document.querySelector('log-window')
@@ -199,7 +200,7 @@ exportButton.addEventListener('click', () => {
 })
 
 // send button - or you can right-click the canvas -> save image
-const sendButton = document.querySelector('.send')
+const sendButton = document.querySelector('.send-button')
 sendButton.addEventListener('click', async () => {
 const THRESHOLD = 128;
 
@@ -272,7 +273,7 @@ function sendRow(num, data) {
       rowNum: num,
       data
     }
-      fetch('https://example.com/endpoint', {
+      fetch(`${ENDPOINT}/draw-row`, {
         method: 'POST',
         body: JSON.stringify(payload),
         'content-type': 'application/json'
@@ -282,6 +283,97 @@ function sendRow(num, data) {
 
   })
 }
+
+function sendText(text, x = 0, y = 0, size = 1) {
+  return new Promise((resolve) => {
+    const payload = {
+      x,
+      y,
+      text,
+      size
+    }
+    fetch(`${ENDPOINT}/draw-text`, {
+      method: 'POST',
+      body: JSON.stringify(payload),
+      'content-type': 'application/json'
+    })
+    .then(res => res.text())
+    .then(data => resolve(data));
+  })
+}
+const sendTextButton = document.querySelector('.send-text-button')
+sendTextButton.addEventListener('click', async () => {
+  const text = document.querySelector('.send-text-text').value
+  const size = document.querySelector('.send-text-size').value
+  const x = document.querySelector('.send-text-x').value
+  const y = document.querySelector('.send-text-y').value
+  sendTextButton.disabled = true
+  await sendText(text, x, y, size)
+  sendTextButton.disabled = false
+})
+
+function clearDisplay() {
+  return new Promise((resolve) => {
+    fetch(`${ENDPOINT}/clear-display`)
+    .then(res => resolve('cleared'));
+  })
+}
+const clearDisplayButton = document.querySelector('.clear-display-button')
+clearDisplayButton.addEventListener('click', async () => {
+  clearDisplayButton.disabled = true
+  await clearDisplay()
+  clearDisplayButton.disabled = false
+})
+
+function invert() {
+  return new Promise((resolve) => {
+    fetch(`${ENDPOINT}/invert`)
+    .then(res => resolve('inverted'));
+  })
+}
+const invertButton = document.querySelector('.invert-button')
+invertButton.addEventListener('click', async () => {
+  invertButton.disabled = true
+  await invert()
+  invertButton.disabled = false
+})
+function unInvert() {
+  return new Promise((resolve) => {
+    fetch(`${ENDPOINT}/un-invert`)
+    .then(res => resolve('un-inverted'));
+  })
+}
+const unInvertButton = document.querySelector('.un-invert-button')
+unInvertButton.addEventListener('click', async () => {
+  unInvertButton.disabled = true
+  await unInvert()
+  unInvertButton.disabled = false
+})
+
+function dim() {
+  return new Promise((resolve) => {
+    fetch(`${ENDPOINT}/dim`)
+    .then(res => resolve('dimmed'));
+  })
+}
+const dimButton = document.querySelector('.dim-button')
+dimButton.addEventListener('click', async () => {
+  dimButton.disabled = true
+  await dim()
+  dimButton.disabled = false
+})
+function unDim() {
+  return new Promise((resolve) => {
+    fetch(`${ENDPOINT}/un-dim`)
+    .then(res => resolve('un-dimmed'));
+  })
+}
+const unDimButton = document.querySelector('.un-dim-button')
+unDimButton.addEventListener('click', async () => {
+  unDimButton.disabled = true
+  await unDim()
+  unDimButton.disabled = false
+})
 
 /** see https://www.npmjs.com/package/sequential-promise-all */
 /**
